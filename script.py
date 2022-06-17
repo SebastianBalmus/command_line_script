152df29d8c152c7dddd63414127719f34f1af057
Sebastian-Olav BALMUS <sbalmus@pentalog.com>
2022-06-17 09:57:56 +0300

    feedback solved

--- script.py
+++ script.py
@@ -7,38 +7,46 @@ from zipfile import ZipFile
 import operator
 import colorama
 import datetime
+import sys
 
-# No abbreviations will be allowed, due to the similarity of some words
-arg_parser = argparse.ArgumentParser(allow_abbrev=False)
 
-# Optional sort argument
-arg_parser.add_argument('--sort', '-so',
-                        metavar='column',
+arg_parser = argparse.ArgumentParser()
+possible_actions = ['sort', 'search', 'filter', 'count']
+
+# Set the script action
+arg_parser.add_argument('action',
+                        metavar='action',
                         type=str,
                         action='store',
-                        help='Sort by one of the columns of the CSV fie')
+                        choices=possible_actions,
+                        help='sort = Sort by one of the columns of the CSV file.'
+                             'search = Find all the occurrences of a term in'
+                             ' the CSV file and highlight them in the terminal.'
+                             'filter = keep only the rows that contain dates in a specified interval.'
+                             'count = Count all the occurrences of a term in the CSV file and display the result.')
 
-# Optional search argument
-arg_parser.add_argument('--search', '-s',
-                        metavar='term',
+# Every action has a minimum of one argument
+arg_parser.add_argument('action_param',
+                        metavar='action_param',
                         type=str,
                         action='store',
-                        help='Find all the occurrences of a term in the CSV file and highlight them in the terminal')
+                        help='Based on the chosen argument, you must add the specified parameter(s).')
 
-# Optional filter argument
-arg_parser.add_argument('--filter', '-f',
-                        nargs=3,
-                        metavar=('column', 'start_date', 'end_date'),
+# In order to actually use the filter action, we will need this
+arg_parser.add_argument('--start_date', '-sd',
+                        metavar='start_date',
                         type=str,
                         action='store',
-                        help='Only the ones that were born or employed in a period will be displayed')
+                        help='if you use the filter function, this will be the lower bound '
+                             'of the interval')
 
-# Optional count argument
-arg_parser.add_argument('--count', '-c',
-                        metavar='term',
+# This one too
+arg_parser.add_argument('--end_date', '-ed',
+                        metavar='end_date',
                         type=str,
                         action='store',
-                        help='Count all the occurrences of a term in the CSV file and display the result')
+                        help='if you use the filter function, this will be the upper bound '
+                             'of the interval')
 
 # Optional limit argument
 arg_parser.add_argument('--limit', '-lim',
@@ -67,57 +75,55 @@ for i in range(len(rows)):
     rows[i][8] = datetime.datetime.strptime(rows[i][8], "%Y-%m-%d").strftime("%d-%m-%Y")
 
 # Code for sorting the data
-if args.sort is not None:
-    if args.sort not in header:
-        print("There is no column called " + args.sort)
+if args.action == 'sort':
+    if args.action_param not in header:
+        print("There is no column called " + args.action_param)
     else:
         # Sort by the header index of the item
-        rows = sorted(rows, key=operator.itemgetter(header.index(args.sort)))
+        rows = sorted(rows, key=operator.itemgetter(header.index(args.action_param)))
         for i in range(0, args.limit):
             print(rows[i])
 
 # Code for searching in the data
-elif args.search is not None:
-    print(args.search)
+elif args.action == 'search':
     row_content = ''
+    count_of_rows = 0
     colorama.init()
-    # We don't want to append a useless newline at the end
-    for i in range(0, args.limit):
-        if i == args.limit - 1:
-            row_content = row_content + (str(rows[i]))
-        else:
-            row_content = row_content + (str(rows[i])) + "\n"
+    for row in rows:
+        if args.action_param in row:
+            row_content = row_content + str(row) + '\n'
+            count_of_rows += 1
+        if count_of_rows is args.limit:
+            break
+    row_content = row_content.strip()
     # Highlight only the content of interest, like a CTRL+F
-    row_content = row_content.replace(args.search, colorama.Fore.RED + args.search + colorama.Fore.RESET)
+    row_content = row_content.replace(args.action_param,
+                                      colorama.Fore.RED + args.action_param + colorama.Fore.RESET)
     print(row_content)
 
 # Code for filtering the data
-elif args.filter is not None:
+elif args.action == 'filter':
+    if args.start_date is None and args.end_date is None:
+        print(f'To filter the data, please provide the {colorama.Fore.RED}start date '
+              f'{colorama.Fore.RESET}and {colorama.Fore.RED}end date{colorama.Fore.RESET}')
+        sys.exit()
     result = []
     # Formatting the limits as dates
-    lower_limit = time.strptime(args.filter[1], "%d-%m-%Y")
-    upper_limit = time.strptime(args.filter[2], "%d-%m-%Y")
+    lower_limit = time.strptime(args.start_date, "%d-%m-%Y")
+    upper_limit = time.strptime(args.end_date, "%d-%m-%Y")
     for row in rows:
-        current_iteration_date = time.strptime(row[header.index(args.filter[0])], "%d-%m-%Y")
+        current_iteration_date = time.strptime(row[header.index(args.action_param)], "%d-%m-%Y")
         # Checking if the date is in the desired interval. If it is, append the row to the result
         if lower_limit <= current_iteration_date <= upper_limit:
             result.append(row)
     # Maybe we don't have enough people to reach the args.limit
-    if len(result) < args.limit:
-        max_len = len(result)
-    else:
-        max_len = args.limit
-    for i in range(0, max_len):
+    max_len = min(args.limit, len(result))
+    for i in range(max_len):
         print(result[i])
 
 # Code for counting the number of occurrences of a string in the CSV
-elif args.count is not None:
+elif args.action == 'count':
     counter = 0
     for row in rows:
-        counter += row.count(args.count)
-    print(str(counter) + " occurrences of word" + args.count + " found in the CSV.")
-
-# If we don't do anything with the script, a message must tell us what we can do to find out more
-else:
-    colorama.init()
-    print("To see the functionality of this script, type " + colorama.Fore.RED + "\'py script.py -h\'" + colorama.Fore.RESET)
+        counter += row.count(args.action_param)
+    print(f'{str(counter)} occurrences of word {args.action_param} found in the CSV.')
